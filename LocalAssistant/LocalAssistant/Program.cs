15f3478c310f1a3e5d59732cbc3c533835f57288
StanislavSeregin <stas_seregin@mail.ru>
2025-04-27 15:20:56 +0300

    Initial commit

---
+++ LocalAssistant/LocalAssistant/Program.cs
@@ -0,0 +1,28 @@
+﻿using LocalAssistant.Tools;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace LocalAssistant;
+
+public class Program
+{
+    static Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Logging.AddConsole(consoleLogOptions =>
+        {
+            consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+        });
+
+        builder.Services
+            .AddMcpServer()
+            .WithHttpTransport()
+            .WithTools<EchoTool>();
+
+        var app = builder.Build();
+        app.MapMcp();
+        return app.RunAsync();
+    }
+}
---
+++ LocalAssistant/LocalAssistant/Tools/EchoTool.cs
@@ -0,0 +1,14 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace LocalAssistant.Tools;
+
+[McpServerToolType]
+public class EchoTool
+{
+    [McpServerTool, Description("Echoes the input back to the client.")]
+    public static string Echo(string message)
+    {
+        return "hello " + message;
+    }
+}
