3eb4cfef2e0f6caeea354f1899444e9f09faa4ba
StanislavSeregin <stas_seregin@mail.ru>
2025-06-16 21:48:31 +0300

    Simplify agent and fix ollama multicall

--- src/Assistant/assistant.py
+++ src/Assistant/assistant.py
@@ -1,13 +1,11 @@
 import uuid
-import json
 from typing import Annotated, AsyncGenerator, TypedDict, Sequence
-from langchain_core.messages import BaseMessage, HumanMessage, ToolMessage, SystemMessage
-from langchain_openai import ChatOpenAI
-from langgraph.graph import END, StateGraph
+from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
+from langchain_ollama import ChatOllama
 from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import InMemorySaver
 from langchain_mcp_adapters.client import MultiServerMCPClient
-from langchain_core.runnables import RunnableConfig
+from langgraph.prebuilt import create_react_agent
 
 
 class AgentState(TypedDict):
@@ -23,11 +21,16 @@ class Assistant:
         self.system_prompt = system_prompt
         self.mcp_url = mcp_url
         self.history = []
-        self.tools_by_name = None
-        self.model = None
         self.graph = None
 
     async def initialize(self):
+        model = ChatOllama(
+            base_url=self.open_ai_url,
+            model=self.model_name,
+            api_key=self.api_key,
+            temperature=self.temperature
+        )
+
         mcp_client = MultiServerMCPClient({
             "default_server": {
                 "transport": "sse",
@@ -36,63 +39,25 @@ class Assistant:
         })
 
         tools = await mcp_client.get_tools()
-        self.tools_by_name = {tool.name: tool for tool in tools}
-        self.model = ChatOpenAI(
-            base_url=self.open_ai_url,
-            model=self.model_name,
-            api_key=self.api_key,
-            temperature=self.temperature
-        ).bind_tools(tools)
 
-        self.graph = self._build_graph()
+        self.graph = create_react_agent(
+            model=model,
+            tools=tools,
+            prompt=SystemMessage(self.system_prompt),
+            version="v1",
+            checkpointer=InMemorySaver()
+        )
+
         return self
-
-    def _build_graph(self):
-        async def call_model(state: AgentState, config: RunnableConfig):
-            system_prompt = SystemMessage(self.system_prompt)
-            response = await self.model.ainvoke(
-                [system_prompt] + state["messages"], config)
-            return {"messages": [response]}
-
-        async def tool_node(state: AgentState):
-            outputs = []
-            for tool_call in state["messages"][-1].tool_calls:
-                tool_result = await self.tools_by_name[tool_call["name"]].ainvoke(
-                    tool_call["args"])
-                outputs.append(
-                    ToolMessage(
-                        content=json.dumps(tool_result),
-                        name=tool_call["name"],
-                        tool_call_id=tool_call["id"],
-                    )
-                )
-
-            return {"messages": outputs}
-
-        def should_continue(state: AgentState):
-            messages = state["messages"]
-            last_message = messages[-1]
-            if not last_message.tool_calls:
-                return "end"
-            else:
-                return "continue"
-
-        workflow = StateGraph(AgentState)
-        workflow.add_node("agent", call_model)
-        workflow.add_node("tools", tool_node)
-        workflow.set_entry_point("agent")
-        workflow.add_conditional_edges("agent", should_continue, {
-                                       "continue": "tools", "end": END})
-        workflow.add_edge("tools", "agent")
-        checkpointer = InMemorySaver()
-        return workflow.compile(checkpointer=checkpointer)
 
     async def process_message(self, message: str) -> AsyncGenerator[str, None]:
         if not self.graph:
             raise RuntimeError("Agent not initialized")
 
-        inputs = {"messages": self.history +
-                  [HumanMessage(content=message, name="user")]}
+        inputs = {
+            "messages": self.history + [HumanMessage(content=message, name="user")]
+        }
+
         config = {"configurable": {"thread_id": uuid.uuid4()}}
         async for msg, _ in self.graph.astream(inputs, config=config, stream_mode="messages"):
             if msg.type == "AIMessageChunk":
@@ -117,5 +82,4 @@ class Assistant:
                 )
 
         state_history_iter = self.graph.get_state_history(config)
-        messages_from_graph = next(state_history_iter).values["messages"]
-        self.history = messages_from_graph
+        self.history = next(state_history_iter).values["messages"]
